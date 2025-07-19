ce061321cf74728f00b48849b5154753a9ad7a30
yunun0410 <yunun0410@gmail.com>
2025-07-19 17:08:04 +0900

    models.py question클래스 작성

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Question(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_id = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    sqe = db.Column(db.Integer, nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime)
+    update_at = db.Column(db.DateTime)
+
