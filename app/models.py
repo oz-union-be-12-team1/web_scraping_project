d61914a8413a3d032334d573574727dc89d4239a
yunun0410 <yunun0410@gmail.com>
2025-07-21 08:17:17 +0900

    models.py 오류 수정

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Question(db.Model):
 class Choices(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.varchar, nullable=False)
+    content = db.Column(db.String(255), nullable=False)
     sqe = db.Column(db.Integer, nullable=False)
     is_active = db.Column(db.Boolean, nullable=False, default=True)
     created_at = db.Column(db.DateTime)
