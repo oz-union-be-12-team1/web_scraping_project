ce0f6140dd90fa077bb87f7260386ee3b1f0f4ed
yunun0410 <yunun0410@gmail.com>
2025-07-21 08:43:23 +0900

    choices.py 선택지 수정 생성

--- app/routes/choices.py
+++ app/routes/choices.py
@@ -1,11 +1,12 @@
 from flask import Flask, Blueprint, jsonify, abort, request
 from config import db
 from app.models import Choices, Question
+from datetime import datetime
 
-choices_bp = Blueprint("choices", __name__, url_prefix='/choice')
+choices_blp = Blueprint("choices", __name__, url_prefix='/choice')
 
 # 선택지 생성
-@choices_bp.route('', methods=['POST'])
+@choices_blp.route('', methods=['POST'])
 def create_choice():
     data = request.get_json()
 
@@ -29,3 +30,28 @@ def create_choice():
         "sqe" : new_choice.sqe,
         "is_active" : new_choice.is_active
     })
+
+# 선택지 수정
+@choices_blp.route('/update/<int:choice_id>', methods=["GET","POST"])
+def update_choice(choice_id):
+    update_choice = Choices.query.get(choice_id)
+    if not update_choice:
+        return jsonify({"message" : "해당 질문이 없습니다."}), 404
+    
+    data = request.get_json()
+
+    for field in ['content', 'is_active', 'sqe', 'question_id']:
+        if field in data:
+            setattr(update_choice, field, data[field])
+
+    update_choice.update_at = datetime.utcnow()
+    update_choice.session.commit()
+
+    return jsonify({
+        "id" : update_choice.id,
+        "question_id" : update_choice.question_id,
+        "content" : update_choice.content,
+        "sqe" : update_choice.sqe,
+        "is_active" : update_choice.is_active
+    })
+
