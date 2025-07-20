74a499aafd04256def367e00202d76154bb9f0b2
kdu0312 <thery91@naver.com>
2025-07-21 04:56:07 +0900

    🐛 users.py 수정

--- app/models.py
+++ app/models.py
@@ -54,3 +54,33 @@ class Image(db.Model):
     created_at = db.Column(db.DateTime, default=now_kst)
     updated_at = db.Column(db.DateTime, default=now_kst, onupdate=now_kst)
 
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    sqe = db.Column(db.Integer, nullable=False, unique=True)
+    is_active = db.Column(db.Boolean, default=True)
+
+    image_id = db.Column(db.Integer, db.ForeignKey('images.id'), nullable=True)
+    image = db.relationship('Image', backref='questions', lazy=True)
+
+    created_at = db.Column(db.DateTime, default=now_kst)
+    updated_at = db.Column(db.DateTime, default=now_kst, onupdate=now_kst)
+
+    choices = db.relationship('Choices', backref='question', lazy=True)
+
+class Choices(db.Model):
+    __tablename__ = 'choices'
+
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
+    sqe = db.Column(db.Integer, nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+    created_at = db.Column(db.DateTime, default=now_kst)
+    updated_at = db.Column(db.DateTime, default=now_kst, onupdate=now_kst)
+
+    answers = db.relationship('Answer', backref='choice', lazy=True)
+
